5f7908a2e12022f0b36f2141533482e186eca56c
lieluzan3 <lieluzan3@gmail.com>
2024-12-02 22:05:40 +0200

    submission1

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -1,3 +1,5 @@
+
+
 /** A library of operations on arrays of characters (char values).
  *  The library also features a string comparison method. */
 public class ArrCharOps {
@@ -5,21 +7,21 @@ public class ArrCharOps {
         String str = "clearly";
         char[] arr1 = {'c','l','e','a','r','l','y'};
         char[] arr2 = {'U','n','d','e','r','s','t', 'o', 'o', 'd'};
-        System.out.println(str);  // Prints the string
-        println(arr1);            // Prints an array of characters
-        System.out.println(charAt(arr1,2));      
-        System.out.println(indexOf(arr1,'l'));  
-        System.out.println(indexOf(arr1,'l',3)); 
-        System.out.println(lastIndexOf(arr1, 'l'));
-        System.out.println(concat(arr1, arr2));
-        System.out.println(subArray(arr2, 2, 9));
-        System.out.println(compareTo("abcd", "abcd"));
-        System.out.println(compareTo("abc", "abcd"));
-        System.out.println(compareTo("abw", "abcd"));
-        System.out.println(compareTo("Abcd", "a"));
-        System.out.println(compareTo("apple", "banana"));
-        System.out.println(compareTo("apple", "applepie"));
-        System.out.println(compareTo("Zoo", "zoo"));
+        //System.out.println(str);  // Prints the string
+       // println(arr1);            // Prints an array of characters
+       // System.out.println(charAt(arr1,2));      
+      //  System.out.println(indexOf(arr1,'l'));  
+       // System.out.println(indexOf(arr1,'l',3)); 
+       // System.out.println(lastIndexOf(arr1, 'l'));
+       // System.out.println(concat(arr1, arr2));
+       // System.out.println(subArray(arr2, 2, 9));
+        //System.out.println(compareTo("abcd", "abcd"));
+       // System.out.println(compareTo("abc", "abcd"));
+        //System.out.println(compareTo("abw", "abcd"));
+        //System.out.println(compareTo("Abcd", "a"));
+        //.out.println(compareTo("apple", "banana"));
+        //System.out.println(compareTo("apple", "applepie"));
+        //System.out.println(compareTo("Zoo", "zoo"));
         System.out.println(hashCode(arr1));
         System.out.println(hashCode(arr2));
     }
@@ -36,7 +38,10 @@ public class ArrCharOps {
     /** Returns the char value at the specified index. Assume that the array is non-empty.
      */
     public static char charAt(char[] arr, int index) {
-        // Replace the following statement with your code
+       if(arr.length<index-1)
+       return arr[index];
+       else
+       
         return 0;
     }
 
@@ -44,38 +49,93 @@ public class ArrCharOps {
      *  returns true; Otherwise returns false.
      */
     public static boolean equals(char[] arr1, char[] arr2) {
-        // Replace the following statement with your code
-        return false;
+        if(arr1.length!=arr2.length){
+            return false;
+        }
+        else{
+            for (int i = 0; i < arr1.length; i++) {
+                if(arr1[i]!=arr2[1])
+                return false;
+            
+            }
+            return true;
+        }
+
+       
     }
 
     /** Returns the index within the given array of the first occurrence of the given character.
      *  If no such character is found, returns -1.
      */
     public static int indexOf(char[] arr, char ch) {
-        // Replace the following statement with your code
+        if(arr.length==0){
         return -1;
+        }
+        else{
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i]==ch){
+                    return i;
+                }
+             
+            }
+            return -1; 
+        }
     }
 
     /** Same as indexOf(char[], char), but starts the search in the given index.
      */
     public static int indexOf(char[] arr, char ch, int fromIndex) {
-        // Replace the following statement with your code
-        return -1;
+        if(arr.length-fromIndex==0){
+            return -1;
+            }
+            else{
+                for (int i = fromIndex; i < arr.length; i++) {
+                    if(arr[i]==ch){
+                        return i;
+                    }
+                 
+                }
+                return -1; 
+            }
+       
     }
 
     /** Returns the index within the given arr of the last occurrence of the given character.
      *  If no such character is found, returns -1.
      */
     public static int lastIndexOf(char[] arr, char ch) {
-        // Replace the following statement with your code
+        if(arr.length==0){
         return -1;
+        }
+        else{
+            int x=-1;
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i]==ch)
+                x=i;
+            }
+            return x;
+
+        }
     }
 
     /* Returns an array which is the concatanation of the two given arrays.
     */
     public static char[] concat(char[] arr1, char[] arr2) {
-        // Replace the following statement with your code
+        char[] arr3=new char[arr1.length+arr2.length];
+        if(arr2.length+arr2.length==0)
         return null;
+        else{
+        for (int i = 0; i < arr1.length; i++) {
+            arr3[i]=arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            arr3[arr1.length+i]=arr2[i];
+            
+        }
+        return arr3;
+
+    }
+        
     }
 
     /** Returns a new array that can be described as a sub-array of this array.
@@ -84,8 +144,19 @@ public class ArrCharOps {
      *  characters containing the characters "urge".
      */     
     public static char[] subArray(char[] arr, int beginIndex, int endIndex) {
-        // Replace the following statement with your code
-        return null;
+        char[] arr2=new char[endIndex-beginIndex+1];
+        if(arr.length==0){
+            return null;
+        }
+        else{
+            for (int i = beginIndex; i < endIndex; i++) {
+                arr2[i]=arr[i];
+                
+            }
+            return arr2;
+            
+        }
+
     }
 
      /** Returns a single integer that represents the given array. This integer is sometimes 
@@ -96,8 +167,12 @@ public class ArrCharOps {
      *  The hash value of an empty array is zero.
      */
     public static long hashCode(char[] arr) {
-        // Replace the following statement with your code
-        return 0;
+        int sum=0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i]*7^arr.length-(i+1);
+        }
+        return sum;
     }
 
     /**
@@ -126,7 +201,28 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        // Replace the following statement with your code
-        return 0;
+        if(str1.length()<str2.length())
+        return -1;
+        else if(str1.length()==str2.length()){
+            for (int i = 0; i < str1.length(); i++) {
+                char char1=str1.charAt(i);
+                char char2=str1.charAt(i);
+                if(char1<char2){
+                    return -1;
+                }
+                if(char1>char2){
+                    return 1;
+                }
+                else 
+                return 0;
+            }
+            }
+            else if (str1.length()>str2.length()) {
+                return 1;
+            }
+            return -2;
+        }
+        
+
+    
     }
-}
--- Primes.java
+++ Primes.java
@@ -1,5 +1,21 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n= Integer.parseInt(args[0]);
+        System.out.println("Prime numbers up to "+n+":");
+       
+            System.out.println(2);
+            
+        for (int i = 3; i < n+1; i++) {
+            int count=0;
+            for (int j = 2; j < i; j++) {
+                if(i%j==0){
+                count++;
+                
+            }
+        }
+        if(count==0)
+        System.out.println(i);
     }
+    
+}
 }
